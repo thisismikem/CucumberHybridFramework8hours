934adbba88202d230dc2692b610354f2439197c1
MICHAELLAPTOP\maima <maimao@gmail.com>
2023-11-16 19:48:12 -0500

    my commit

---
+++ src/test/java/factory/DriverFactory.java
@@ -0,0 +1,63 @@
+package factory;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariDriver;
+
+import utils.CommonUtils;
+
+public class DriverFactory {
+	
+	static WebDriver driver = null;
+	
+	public static WebDriver initializeBrowser(String browserName) {
+		
+		if(browserName.equals("chrome")) {
+			ChromeOptions options = new ChromeOptions();
+	        options.addArguments("headless");
+	        options.addArguments("window-size=1400,800");       
+	        options.addArguments("disable-gpu");
+	        
+			driver = new ChromeDriver(options);
+			
+		}else if(browserName.equals("firefox")) {
+//			FirefoxBinary firefoxBinary = new FirefoxBinary();
+//			FirefoxOptions options = new FirefoxOptions();
+//			options.setBinary(firefoxBinary);
+//			options.addArguments("-headless");
+			
+			driver = new FirefoxDriver();
+			
+		}else if(browserName.equals("edge")) {
+			
+			driver = new EdgeDriver();
+			
+		}else if(browserName.equals("safari")) {
+			
+			driver = new SafariDriver();
+			
+		}
+		
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(CommonUtils.PAGE_LOAD_TIME));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(CommonUtils.IMPLICIT_WAIT_TIME));
+		
+		return driver;
+		
+	}
+	
+	public static WebDriver getDriver() {
+		
+		return driver;
+		
+	}
+
+}
---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,17 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/features",
+				 glue={"stepdefinitions","hooks"},
+				 publish=true,
+				 plugin={
+						 "pretty",
+						 "html:target/CucumberReports/report.html",
+						 "json:target/CucumberReports/report.json"
+						})
+public class TestRunner {}
